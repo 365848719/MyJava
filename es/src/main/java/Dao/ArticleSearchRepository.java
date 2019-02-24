8173adf03a94af50d8e5260f42583e1d146e1f84
365848719 <44990364+365848719@users.noreply.github.com>
2019-02-24 22:47:47 +0800

    添加spring boot 的es

---
+++ es/src/main/java/Dao/ArticleSearchRepository.java
@@ -0,0 +1,9 @@
+package Dao;
+
+
+import Entiry.Article;
+import org.springframework.data.elasticsearch.repository.ElasticsearchRepository;
+
+public interface ArticleSearchRepository extends ElasticsearchRepository<Article, Long> {
+
+}
